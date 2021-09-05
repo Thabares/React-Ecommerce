92f552a08b5bb7c6dbebd0b55e6f222c30f2c2b9
Thabares <thabares786@gmail.com>
2021-09-05 09:14:03 +0530

    newsletter added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Announcement from '../components/Announcement';
 import Categories from '../components/Categories';
 import Navbar from '../components/Navbar';
 import Products from '../components/Products';
+import Newsletter from '../components/Newsletter';
 import Slider from '../components/Slider';
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
       <Slider />
       <Categories />
       <Products />
+      <Newsletter />
     </div>
   );
 };

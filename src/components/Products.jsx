3332c178a0f99d2351c014b309be09b80a0f19cb
Thabares Nazeer Basha <thabaresnazeer.1995@gmail.com>
2022-04-03 11:07:11 +0530

    listing products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
 import styled from 'styled-components';
 import { popularProducts } from '../data';
 import Product from './Product';
@@ -9,10 +11,35 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
-const Products = () => {
+const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+
+        const res = await axios.get(cat
+          ? `http://localhost:5000/api/products?category=${cat}` : 'http://localhost:5000/api/products')
+
+        setProducts(res.data)
+      } catch (err) { console.log(err) }
+    }
+    getProducts()
+  }, [cat])
+
+
+  useEffect(() => {
+    cat && setFilteredProducts(
+      products.filter((item) => Object.entries(filters).every(([key, value]) =>
+        item[key].includes(value)
+      ))
+    )
+  }, [products, cat, filters])
+
   return (
     <Container>
-      {popularProducts.map((item) => (
+      {filteredProducts.map((item) => (
         <Product item={item} key={item.id} />
       ))}
     </Container>
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import Announcement from '../components/Announcement';
 import Footer from '../components/Footer';
@@ -38,6 +40,20 @@ const Select = styled.select`
 const Option = styled.option``;
 
 const ProductList = () => {
+
+  const location = useLocation()
+  const cat = location.pathname.split("/")[2];
+  const [filters, setFilters] = useState({})
+  const [sort, setSort] = useState("newest")
+
+  const handleFilters = (event) => {
+    setFilters({
+      ...filters,
+      [event.target.name]: event.target.value
+    })
+  }
+
+
   return (
     <Container>
       <Navbar />
@@ -47,8 +63,8 @@ const ProductList = () => {
       <FilterContainer>
         <Filter>
           <FilterText>Filter Products</FilterText>
-          <Select>
-            <Option disabled selected>
+          <Select name="color" onChange={handleFilters}>
+            <Option disabled>
               Color
             </Option>
             <Option>White</Option>
@@ -58,8 +74,8 @@ const ProductList = () => {
             <Option>Yellow</Option>
             <Option>Green</Option>
           </Select>
-          <Select>
-            <Option disabled selected>
+          <Select name="size" onChange={handleFilters}>
+            <Option disabled>
               Size
             </Option>
             <Option>XS</Option>
@@ -71,15 +87,15 @@ const ProductList = () => {
         </Filter>
         <Filter>
           <FilterText>Sort Products</FilterText>
-          <Select>
-            <Option selected>Newest</Option>
-            <Option>Price (asc)</Option>
-            <Option>Price (dsc)</Option>
+          <Select onChange={(e) => { setSort(e.target.value) }}>
+            <Option selected value="newest">Newest</Option>
+            <Option value="asc">Price (asc)</Option>
+            <Option value="desc">Price (dsc)</Option>
           </Select>
         </Filter>
       </FilterContainer>
 
-      <Products />
+      <Products cat={cat} filters={filters} sort={sort} />
       <NewsLetter />
       <Footer />
     </Container>
